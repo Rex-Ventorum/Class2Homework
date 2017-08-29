a0ba0f8f2edfcdfa30b0edb7e59ceb8c4e4c46ce
Brandon <Brandon@NubCenter.attlocal.net>
2017-08-29 13:44:14 -0500

    Cleaned Up Auto Generate

--- src/class2homework/Person.java
+++ src/class2homework/Person.java
@@ -2,31 +2,23 @@ package class2homework;
 
 public class Person {
 
-     private String firstName;
+    private String firstName;
     private String lastName;
     private int age; 
     
-    public String getFirstName() {
-        return firstName;
-    }
+    //------------------------//
+    //---- GETTER METHODS ----//
+    //------------------------//
+    
+    public String getFirstName() {return firstName;}
+    public String getLastName() { return lastName;}
+    public int getAge() { return age;}
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }    
+    //------------------------//
+    //---- SETTER METHODS ----//
+    //------------------------//
+    
+    public void setFirstName(String firstName) {this.firstName = firstName;}
+    public void setLastName(String lastName) { this.lastName = lastName; }
+    public void setAge(int age) { this.age = age;}    
 }
