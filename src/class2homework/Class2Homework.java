14a0f60609dcb835d524ec94e31a31cf2c63f360
Brandon <Brandon@NubCenter.attlocal.net>
2017-08-29 13:56:59 -0500

    Failed Revert Attempt

--- src/class2homework/Class2Homework.java
+++ src/class2homework/Class2Homework.java
@@ -14,7 +14,7 @@ public class Class2Homework {
         maryObject.setAge(28);
         
         System.out.println(johnObject.getFullName() + " is " + johnObject.getAge() + " years old");
-        System.out.println(maryObject.getFullName() + " is " + maryObject.getAge() + " years old");    
+        System.out.println(maryObject.getFullName() + " is " + maryObject.getAge() + " years old");   
     }
     
 }
