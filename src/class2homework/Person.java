6254e531781f911fae4b20826e4edfa6266ce022
Brandon <Brandon@NubCenter.attlocal.net>
2017-08-29 13:41:20 -0500

    Auto Generated Getters and Setters Using Netbeans "Alt+Insert"

--- src/class2homework/Person.java
+++ src/class2homework/Person.java
@@ -1,8 +1,32 @@
 package class2homework;
 
 public class Person {
-    private String firstName;
+
+     private String firstName;
     private String lastName;
     private int age; 
     
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }    
 }
