3511c0203e003b1799490049144c10fa7f8df338
Brandon <Brandon@NubCenter.attlocal.net>
2017-08-29 13:48:51 -0500

    Created John and Mary Person Objects and Output Their Info

--- src/class2homework/Class2Homework.java
+++ src/class2homework/Class2Homework.java
@@ -3,7 +3,20 @@ package class2homework;
 public class Class2Homework {
 
     public static void main(String[] args) {
-
+        Person johnObject = new Person();
+        johnObject.setFirstName("John");
+        johnObject.setLastName("Smith");
+        johnObject.setAge(30);
+        
+        Person maryObject = new Person();
+        maryObject.setFirstName("Mary");
+        maryObject.setLastName("Smith");
+        maryObject.setAge(28);
+        
+        System.out.println(johnObject.getFirstName() + " " + johnObject.getLastName() 
+                + " is " + johnObject.getAge() + " years old");
+        System.out.println(maryObject.getFirstName() + " " + maryObject.getLastName() 
+                + " is " + maryObject.getAge() + " years old");    
     }
     
 }
